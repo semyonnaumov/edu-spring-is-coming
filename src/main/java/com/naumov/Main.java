4b23f47a4cc319843132f4472802652d077ff3ab
Semyon Naumov <Naumov.S.Serg@sberbank.ru>
2021-08-02 23:07:53 +0300

    Lesson 7: change musicPlayer scope to prototype

--- src/main/java/com/naumov/Main.java
+++ src/main/java/com/naumov/Main.java
@@ -6,9 +6,15 @@ public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        MusicPlayer player = context.getBean("musicPlayer", MusicPlayer.class);
-        player.playMusic();
-        player.showAvailable();
+        MusicPlayer player0 = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer player1 = context.getBean("musicPlayer", MusicPlayer.class);
+
+        System.out.println(player0 == player1); // false since prototype
+
+        Music classicalMusic0 = context.getBean("classicalMusic", Music.class);
+        Music classicalMusic1 = context.getBean("classicalMusic", Music.class);
+
+        System.out.println(classicalMusic0 == classicalMusic1); // true since singleton
 
         context.close();
     }
